698663603604f59a4a37da5c53888d5c710d1f97
gomathishankar <gomathishankarbe@gmail.com>
2025-03-30 12:52:45 +0200

    Adding Refresh Configuration from Git

--- loans/src/main/java/org/gslearn/loans/dto/LoansApplicationDto.java
+++ loans/src/main/java/org/gslearn/loans/dto/LoansApplicationDto.java
@@ -1,10 +1,17 @@
 package org.gslearn.loans.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "loans")
-public record LoansApplicationDto(String message, Map<String,String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class LoansApplicationDto{
+    private String message;
+    private Map<String,String> contactDetails;
+    private List<String> onCallSupport;
 }
