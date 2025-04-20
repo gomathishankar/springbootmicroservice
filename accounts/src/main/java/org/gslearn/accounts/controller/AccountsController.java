81d1595a936b26e820aa6974d7c166e4ec38324d
gomathishankar <gomathishankarbe@gmail.com>
2025-04-20 05:47:41 +0200

    Docker update and RateLimiter

--- accounts/src/main/java/org/gslearn/accounts/controller/AccountsController.java
+++ accounts/src/main/java/org/gslearn/accounts/controller/AccountsController.java
@@ -1,5 +1,6 @@
 package org.gslearn.accounts.controller;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -201,9 +202,15 @@ public class AccountsController {
             )
     }
     )
+    @RateLimiter(name="getJavaVersion",fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity.status(HttpStatus.OK).body(env.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        logger.debug("getJavaVersionFallback() method invoked" );
+        return ResponseEntity.status(HttpStatus.OK).body("17");
     }
 
     @Operation(
